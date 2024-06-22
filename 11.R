947f135c56ad959a078c37b10668d09a77740c97
rougepavan <130356371+rougepavan@users.noreply.github.com>
2024-06-22 20:44:51 +0530

    Add files via upload

--- 11.R
+++ 11.R
@@ -2,8 +2,8 @@ names <- c("siri", "mahi", "chiru")
 age <- c(23, 24, 25)
 marks <- c(88, 78, 25)
 df <- data.frame(names, age, marks)
-
 mid_range <- (min(df$age) + max(df$age)) / 2
 print(mid_range)
 
-write.csv(df, "datafr.csv")
+
+
